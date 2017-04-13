5b1c1db1744daa62f9593586f187946f2842c484
Risheng Fang <rishengf@student.unimelb.edu.au>
2017-04-13 15:06:31 +1000

    create resource class

---
+++ src/com/unimelb/comp90015/fourLiterGroup/ezshare/serverOps/Resource.java
@@ -0,0 +1,14 @@
+package com.unimelb.comp90015.fourLiterGroup.ezshare.serverOps;
+
+public class Resource {
+	private String Name = null;
+	private String Description = null;
+	private String[] Tags = null;
+	private String URI = null;
+	private String Channel = null;
+	private String Owner = null;
+	private String EZserver = null;
+	
+	public Resource(){//Constructor
+	}
+}
